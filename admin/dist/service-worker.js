c786a0795428a09af02df8e3c182506074ce195f
&rey Nevinny <62385969+psoglav@users.noreply.github.com>
2020-09-10 18:19:19 +0600

    deposits

--- admin/dist/service-worker.js
+++ admin/dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.1eddd918c65f7d4306fdcca6b376c476.js", "/workbox-v4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.147db43d5e7eb1c8e900b3e62f3aed1f.js", "/workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/workbox-v4.3.1"});
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.precaching.suppressWarnings()
