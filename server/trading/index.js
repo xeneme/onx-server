d0da73303f5c3b69f5cecd8a7de5633e52a2ca60
Andrey Nevinny <psoglav.ih8u@gmail.com>
2021-02-21 21:30:10 +0600

    trading: increased price update rate

--- server/trading/index.js
+++ server/trading/index.js
@@ -272,7 +272,7 @@ const defineIO = value => {
 
 Graph.updateRealHistory()
 setInterval(Graph.applyFakedHistory, 4000)
-setInterval(Orders.updatePrice, 900)
+setInterval(Orders.updatePrice, 5000)
 
 setInterval(() => {
   lobbies.forEach(lobby => {
