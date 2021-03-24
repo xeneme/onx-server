ad1221c9e0b8c31b6e529b9c86627c00e61ae80e
Andrey Nevinny <psoglav.ih8u@gmail.com>
2021-03-25 02:04:36 +0600

    some trading fixes

--- server/trading/index.js
+++ server/trading/index.js
@@ -49,7 +49,7 @@ const Helper = {
   },
   minusPlus: () => (Math.random() < 0.5 ? 1 : -1),
   randomDelay() {
-    return Math.random() * 1000 + 100
+    return Math.random() * 350 + 50
   },
 }
 
@@ -245,16 +245,55 @@ const Orders = {
     if (Data.orders.XRP.length > 100) Data.orders.XRP.pop()
   },
   placeNewOrder(currency) {
+    const randomDict = {
+      'bitcoin': {
+        min: 0.003,
+        mid: 0.08,
+        max: 0.23,
+        zeros: 6
+      },
+      'litecoin': {
+        min: 0.05,
+        mid: 4,
+        max: 12.5,
+        zeros: 5
+      },
+      'ethereum': {
+        min: 0.002,
+        mid: 2,
+        max: 5.2,
+        zeros: 5
+      },
+      'polkadot': {
+        min: 0.03,
+        mid: 5,
+        max: 15,
+        zeros: 2
+      },
+      'chainlink': {
+        min: 0.02,
+        mid: 4,
+        max: 20,
+        zeros: 2
+      },
+      'ripple': {
+        min: 4,
+        mid: 80,
+        max: 500,
+        zeros: 1
+      },
+    }
+
     var price = priceList[currency]
     var amount = (Math.random() * Math.random()) / 10
     var t = new Date()
 
-    if (Math.random() > 0.9) amount += 1
-    else if (Math.random() > 0.9) amount += 0.1
+    if (Math.random() > 0.8) amount = randomDict[currency].min + Math.random() * randomDict[currency].mid
+    else amount = randomDict[currency].min + Math.random() * randomDict[currency].max
 
     return {
       price,
-      amount: amount.toFixed(10),
+      amount: amount.toFixed(randomDict[currency].zeros),
       time:
         (t.getHours() < 10 ? '0' + t.getHours() : t.getHours()) +
         ':' +
