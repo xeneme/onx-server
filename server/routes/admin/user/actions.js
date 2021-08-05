61a9679d34cc6a113bfa87ebc4a96a1167872003
Andrey Nevinny <psoglav.ih8u@gmail.com>
2021-08-05 21:17:49 +0300

    separate user & actions from admin route

---
+++ server/routes/admin/user/actions.js
@@ -0,0 +1,378 @@
+const express = require('express')
+const router = express.Router()
+
+const { requirePermissions } = require('../index')
+
+const Role = require('../../../user/roles')
+const UserToken = require('../../../user/token')
+const Binding = require('../../../manager/binding')
+
+const User = require('../../../models/User')
+
+
+const sendPopup = (user, res, type, title, text) => {
+  return new Promise((resolve, reject) => {
+    User.findById(user, (err, user) => {
+      if (
+        user &&
+        user.role.name == 'user' &&
+        (res.locals.binded.includes(user.email) ||
+          res.locals.user.role.name == 'owner')
+      ) {
+        user.popup = {
+          type,
+          title,
+          text,
+        }
+
+        user.save(() => {
+          resolve(user)
+        })
+      } else {
+        reject()
+      }
+    })
+  })
+}
+
+
+router.get(
+  '/user/bind',
+  requirePermissions('write:users.binded'),
+  (req, res) => {
+    Binding.set(
+      { by: req.query.by, manager: res.locals.user },
+      (error, success) => {
+        if (error) {
+          res.status(400).send({ message: error })
+        } else {
+          res.send({ message: success })
+        }
+      },
+    )
+  },
+)
+
+router.get(
+  '/user/bind/:user/to/:manager',
+  requirePermissions('write:users.all'),
+  (req, res) => {
+    if (req.params.user && req.params.manager) {
+      User.findById(req.params.manager, (err, manager) => {
+        Binding.setFor(
+          { userid: req.params.user, manager },
+          (error, success) => {
+            if (error) {
+              res.status(400).send({ message: error })
+            } else {
+              res.send({ message: success })
+            }
+          },
+        )
+      }).lean()
+    } else {
+      res.status(400).send({
+        message: 'Requisites are not received',
+      })
+    }
+  },
+)
+
+router.get(
+  '/user/:id/signin',
+  requirePermissions('read:users.binded'),
+  (req, res) => {
+    User.findById(req.params.id, (err, user) => {
+      const success =
+        !!user &&
+        (res.locals.binded.includes(user.email) ||
+          res.locals.user.role.name == 'owner')
+
+      res.send({
+        success,
+        admin: success ? UserToken.authorizationToken(res.locals.user) : '',
+        token: success ? UserToken.authorizationToken(user, true) : '',
+        action: 'sign in as',
+      })
+    }).lean()
+  },
+)
+
+router.get(
+  '/user/:id/general-chat/:mode',
+  requirePermissions('write:users.binded'),
+  (req, res) => {
+    User.findById(req.params.id, (err, user) => {
+      if (user) {
+        user.generalChat = req.params.mode == 'on'
+        user.save(() => {
+          res.send({
+            success: true,
+            action: 'general-chat',
+            message: 'General chat turned ' + req.params.mode
+          })
+        })
+      } else {
+        res.status(404).send({
+          success: false,
+          action: 'general-chat',
+          message: 'User not found.'
+        })
+      }
+    })
+  },
+)
+
+router.get(
+  '/user/:id/ban',
+  requirePermissions('write:users.binded'),
+  (req, res) => {
+    User.findById(req.params.id, (err, user) => {
+      if (
+        user &&
+        ((user.role.name == 'user' && res.locals.binded.includes(user.email)) ||
+          res.locals.user.role.name == 'owner')
+      ) {
+        if (user.banned) {
+          res.send({
+            success: false,
+            message: 'The user is already banned.',
+            action: 'ban',
+          })
+        } else {
+          user.banned = true
+          user.save(() => {
+            res.send({
+              success: true,
+              message: 'The user was banned.',
+              action: 'ban',
+            })
+          })
+        }
+      } else {
+        res.send({
+          success: false,
+          message: 'Could not ban this user.',
+          action: 'ban',
+        })
+      }
+    })
+  },
+)
+
+router.get(
+  '/user/:id/unban',
+  requirePermissions('write:users.binded'),
+  (req, res) => {
+    User.findById(req.params.id, (err, user) => {
+      if (
+        user &&
+        ((user.role.name == 'user' && res.locals.binded.includes(user.email)) ||
+          res.locals.user.role.name == 'owner')
+      ) {
+        if (!user.banned) {
+          res.send({
+            success: false,
+            message: 'The user is not banned.',
+            action: 'unban',
+          })
+        } else {
+          user.banned = false
+          user.save(() => {
+            res.send({
+              success: true,
+              message: 'The user was unbanned.',
+              action: 'ban',
+            })
+          })
+        }
+      } else {
+        res.send({
+          success: false,
+          message: 'Could not unban this user.',
+          action: 'ban',
+        })
+      }
+    })
+  },
+)
+
+router.get(
+  '/user/:id/delete',
+  requirePermissions('write:users.all'),
+  (req, res) => {
+    User.deleteOne({ _id: req.params.id }, (err, result) => {
+      res.send({
+        success: true,
+        action: 'delete',
+      })
+    }).lean()
+  },
+)
+
+router.get(
+  '/user/:id/promote',
+  requirePermissions('write:users.all'),
+  (req, res) => {
+    User.findOne({ _id: req.params.id }, (err, user) => {
+      if (err) {
+        res.send({
+          success: false,
+          action: 'promote',
+        })
+      } else {
+        user.role = Role.promote(user.role.name)
+        user.markModified('role')
+        user.save((err, modified) => {
+          res.send({
+            success: Boolean(modified),
+            action: 'promote',
+          })
+        })
+      }
+    })
+  },
+)
+
+router.get(
+  '/user/:id/demote',
+  requirePermissions('write:users.all'),
+  (req, res) => {
+    User.findOne({ _id: req.params.id }, (err, user) => {
+      if (err) {
+        res.send({
+          success: false,
+          action: 'promote',
+        })
+      } else {
+        user.role = Role.demote(user.role.name)
+        user.markModified('role')
+        user.save((err, modified) => {
+          res.send({
+            success: Boolean(modified),
+            action: 'demote',
+          })
+        })
+      }
+    })
+  },
+)
+
+router.post(
+  '/user/:id/throw_popup',
+  requirePermissions('write:users.binded'),
+  (req, res) => {
+    const { type, title, text } = req.body
+
+    if (title && text && type) {
+      sendPopup(req.params.id, res, type, title, text)
+        .then(user => {
+          res.send(user)
+        })
+        .catch(err => {
+          res.sendStatus(403)
+        })
+    } else {
+      res.sendStatus(400)
+    }
+  },
+)
+
+router.post(
+  '/user/:id/set_withdraw_error',
+  requirePermissions('write:users.binded'),
+  (req, res) => {
+    const { text } = req.body
+
+    User.findById(req.params.id, (err, user) => {
+      if (
+        !user ||
+        (!res.locals.binded.includes(user.email) &&
+          res.locals.user.role.name != 'owner')
+      ) {
+        res.sendStatus(403)
+      } else {
+        user.customWithdrawError = text
+        user.save(() => {
+          res.sendStatus(200)
+        })
+      }
+    })
+  },
+)
+
+router.get(
+  '/user/:id/ban/transfer',
+  requirePermissions('write:users.binded'),
+  (req, res) => {
+    User.findById(req.params.id, (err, user) => {
+      if (
+        user &&
+        ((user.role.name == 'user' && res.locals.binded.includes(user.email)) ||
+          res.locals.user.role.name == 'owner')
+      ) {
+        if (user.banList.includes('transfer')) {
+          res.send({
+            success: false,
+            message: 'The user is already banned from transfer.',
+            action: 'ban-transfer',
+          })
+        } else {
+          user.banList.push('transfer')
+          user.save(() => {
+            res.send({
+              success: true,
+              message: 'The user was banned from transfer.',
+              action: 'ban-transfer',
+            })
+          })
+        }
+      } else {
+        res.send({
+          success: false,
+          message: 'Could not ban this user from transfer.',
+          action: 'ban-transfer',
+        })
+      }
+    })
+  },
+)
+
+router.get(
+  '/user/:id/unban/transfer',
+  requirePermissions('write:users.binded'),
+  (req, res) => {
+    User.findById(req.params.id, (err, user) => {
+      if (
+        user &&
+        ((user.role.name == 'user' && res.locals.binded.includes(user.email)) ||
+          res.locals.user.role.name == 'owner')
+      ) {
+        if (user.banList.includes('transfer')) {
+          user.banList = user.banList.filter(ban => ban != 'transfer')
+          user.save(() => {
+            res.send({
+              success: true,
+              message: 'The user was unbanned from transfer.',
+              action: 'unban-transfer',
+            })
+          })
+        } else {
+          res.send({
+            success: false,
+            message: 'The user is not banned from transfer.',
+            action: 'unban-transfer',
+          })
+        }
+      } else {
+        res.send({
+          success: false,
+          message: 'Could not unban this user from transfer.',
+          action: 'unban-transfer',
+        })
+      }
+    })
+  },
+)
+
+module.exports = router
---
+++ server/routes/admin/user/index.js
@@ -0,0 +1,302 @@
+const express = require('express')
+const router = express.Router()
+
+const { requirePermissions } = require('../index')
+
+const mw = require('../../../user/middleware')
+const UserWallet = require('../../../user/wallet')
+const UserLogger = require('../../../user/logger')
+const Role = require('../../../user/roles')
+const Settings = require('../../../user/admin/settings')
+
+const User = require('../../../models/User')
+const SupportDialogue = require('../../../models/SupportDialogue')
+
+const actionsRoute = require('./actions')
+
+router.use('/', actionsRoute)
+
+
+const getDialogue = (user, read) =>
+  new Promise((resolve, reject) => {
+    SupportDialogue.findOne({ user }, (err, dialogue) => {
+      if (dialogue) {
+        if (read) {
+          dialogue.supportUnread = 0
+          dialogue.save(null)
+        }
+        resolve(dialogue.messages)
+      } else {
+        resolve([])
+      }
+    }).lean(!read)
+  })
+
+
+router.get('/user',
+  requirePermissions(
+    'read:users.all',
+    'read:users.binded',
+    'write:users.binded',
+  ),
+  (req, res) => {
+    if (!req.query.id) res.sendStatus(400)
+    else {
+      const me = res.locals.user
+
+      User.findById(req.query.id, 'email banned banList generalChat role.name wallets at firstName lastName email lastOnline customWithdrawError location bindedTo', (err, user) => {
+        if (err || !user) {
+          res.sendStatus(404)
+        } else {
+          if (
+            !Role.hasPermission(me.role, 'read:users.binded') &&
+            !res.locals.binded.includes(user.email)
+          ) {
+            res.sendStatus(403)
+          } else {
+            const actions = [
+              {
+                nameLocalPath: 'dashboard.profile.tabs.actions.signInAs',
+                color: 'primary',
+                url: `/api/admin/user/${user._id}/signin`,
+              },
+              {
+                nameLocalPath: `dashboard.profile.actions.${user.banned ? 'unban' : 'ban'
+                  }`,
+                color: user.banned ? 'primary' : 'danger',
+                url: `/api/admin/user/${user._id}/${user.banned ? 'unban' : 'ban'
+                  }`,
+              },
+              {
+                nameLocalPath: `dashboard.profile.actions.${user.banList.includes('transfer') ? 'un' : ''
+                  }ban-transfer`,
+                color: user.banList.includes('transfer') ? 'primary' : 'danger',
+                url: `/api/admin/user/${user._id}/${user.banList.includes('transfer') ? 'un' : ''
+                  }ban/transfer`,
+              },
+            ]
+
+            if (user.generalChat) {
+              actions.push(
+                {
+                  nameLocalPath: 'dashboard.profile.tabs.actions.generalChat',
+                  color: 'danger',
+                  url: `/api/admin/user/${user._id}/general-chat/off`,
+                },
+              )
+            } else {
+              actions.push(
+                {
+                  nameLocalPath: 'dashboard.profile.tabs.actions.generalChat',
+                  color: 'primary',
+                  url: `/api/admin/user/${user._id}/general-chat/on`,
+                },
+              )
+            }
+
+            if (
+              Role.hasPermission(me.role, 'write:users.all') &&
+              user.role.name != 'owner'
+            ) {
+              actions.push({
+                nameLocalPath: 'dashboard.profile.actions.deleteUser',
+                color: 'danger',
+                url: `/api/admin/user/${user._id}/delete`,
+              })
+
+              if (user.role.name == 'user') {
+                actions.push({
+                  nameLocalPath: 'dashboard.profile.actions.promote',
+                  color: 'primary',
+                  url: `/api/admin/user/${user._id}/promote`,
+                })
+              } else if (user.role.name == 'manager') {
+                actions.push({
+                  nameLocalPath: 'dashboard.profile.actions.demote',
+                  color: 'danger',
+                  url: `/api/admin/user/${user._id}/demote`,
+                })
+              }
+            }
+
+            var pending = [
+              getDialogue(user._id),
+              UserWallet.getTransactionsByUserId(user._id, false, true),
+              UserLogger.getByUserID(user._id),
+            ]
+
+            Promise.all(pending).then(([messages, transactions, logs]) => {
+              res.send(
+                mw.convertUser(
+                  user,
+                  user.role.name != 'owner' ? actions : [],
+                  logs,
+                  user.wallets,
+                  transactions,
+                  messages,
+                ),
+              )
+            })
+          }
+        }
+      }).lean()
+    }
+  },
+)
+
+router.get('/me', requirePermissions('read:users.self'), (req, res) => {
+  const me = res.locals.user
+
+  UserWallet.getTransactionsByUserId(me._id, false, true).then(transactions => {
+    UserLogger.getByUserID(me._id).then(logs => {
+      var user = mw.convertUser(
+        me,
+        [],
+        logs,
+        me.wallets,
+        transactions,
+        [],
+        true,
+      )
+
+      res.send(user)
+    })
+  })
+})
+
+router.get('/managers',
+  requirePermissions('read:users.all'),
+  async (req, res) => {
+    const managers = (
+      await User.find(
+        { 'role.name': { $in: ['manager', 'owner'] } },
+        req.query.email ? 'email' : undefined,
+      ).lean()
+    ).map(m => req.query.email ? m.email : m)
+
+    res.send(managers)
+  },
+)
+
+router.get('/users',
+  requirePermissions('read:users.all', 'read:users.binded'),
+  async (req, res) => {
+    const page = req.query.page || 0
+    const mode = +req.query.mode || 0
+    const search = req.query.search ? req.query.search.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
+      .replace(/-/g, '\\x2d') : ''
+
+    if (page < 0 || ![0, 1].includes(mode)) {
+      res.status(400).send()
+      return
+    }
+
+    var query = {}
+
+    if (search) {
+      let startsWith = { $regex: new RegExp(`^${search}`) }
+      query.$or = [
+        { _id: startsWith },
+        { email: startsWith },
+        { firstName: startsWith },
+        { lastName: startsWith },
+        { 'wallets.bitcoin.address': startsWith },
+        { 'wallets.litecoin.address': startsWith },
+        { 'wallets.ethereum.address': startsWith },
+      ]
+    }
+
+    if (mode) {
+      query.supportUnread = {
+        $gt: 0
+      }
+    }
+
+    if (Role.hasChain(res, 'read:users.all')) {
+      query._id = {
+        $ne: res.locals.user._id
+      }
+    } else if (Role.hasChain(res, 'read:users.binded')) {
+      query['role.name'] = 'user'
+      query.email = {
+        $in: res.locals.binded.filter(b => typeof b == 'string')
+      }
+    }
+
+    var users = await User.find(query,
+      'at role.name firstName email lastName supportUnread generalUnread lastOnline',
+      { skip: 8 * (Math.max(page, 1) - 1), limit: 8 }
+    )
+      .sort({
+        lastOnline: -1
+      })
+      .lean()
+
+
+    const count = await User.countDocuments(query)
+
+    users = mw.convertUsers(users)
+
+    res.send({ users, totalPages: Math.ceil(count / 8) })
+  },
+)
+
+router.get('/users/binded',
+  requirePermissions('read:users.binded'),
+  (req, res) => {
+    if (res.locals.binded.constructor === Array) {
+      Promise.all([
+        User.find({ email: { $in: res.locals.binded } }).lean(),
+        UserLogger.getAll().lean()
+      ])
+        .then(([users, logs]) => {
+          res.send(mw.convertUsers(users, logs))
+        })
+    } else {
+      res.sendStatus(400)
+    }
+  },
+)
+
+router.get('/actions',
+  requirePermissions('read:actions.binded'),
+  async (req, res) => {
+    if (res.locals.user.role.name == 'owner') {
+      let logs = await UserLogger.getAll()
+
+      res.send({
+        logs: logs || []
+      })
+    } else {
+      let logs = await UserLogger.getBinded(res.locals.binded, true)
+
+      res.send({
+        logs: logs || []
+      })
+    }
+  },
+)
+
+router.get('/actions/binded',
+  requirePermissions('read:actions.binded'),
+  (req, res) => {
+    res.send(UserLogger.getBinded(res.locals.binded))
+  },
+)
+
+router.get('/general-chat', requirePermissions('write:users.binded'), (req, res) => {
+  const user = res.locals.user
+
+  if (!req.query.enabled) {
+    res.send({ enabled: String(user.role?.settings['general-chat']) })
+  } else {
+    const enabled = req.query.enabled == 'true'
+
+    Settings.update(user, 'general-chat', enabled)
+
+    res.send({ message: 'User chat is ' + (enabled ? 'enabled' : 'disabled') })
+  }
+
+})
+
+module.exports = router
